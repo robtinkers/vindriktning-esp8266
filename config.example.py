9cb9ecb4b829e1228bf9de7851fd4ec0181d55ab
robtinkers <74528586+robtinkers@users.noreply.github.com>
2023-02-18 19:02:58 +0200

    config.example.py includes machine_id

--- config.example.py
+++ config.example.py
@@ -3,13 +3,12 @@
 from machine import unique_id
 from ubinascii import hexlify
 
-_machine_id = hexlify(unique_id()).decode()
-#print('MACHINE_ID = %s' % _machine_id)
+machine_id = hexlify(unique_id()).decode()
 
 wifi_network  = 'My-WiFi'
 wifi_password = 'My-Pass'
 
-mqtt_client_id  = _machine_id
+mqtt_client_id  = machine_id
 mqtt_server     = 'mqtt.example.com'
 mqtt_username   = 'mqtt_username'
 mqtt_password   = 'mqtt_password'
@@ -19,5 +18,3 @@ pm1006_rxpin = 13
 pm1006_adjust_add = None # add first
 pm1006_adjust_mul = None # then multiply
 pm1006_smoothing = None # exponential smoothing [0,1.0)
-
-del _machine_id
